8c60e30902f942689a9238582cd26c5a74dd4eef
KarthikReddyVudagandla <37356094+KarthikReddyVudagandla@users.noreply.github.com>
2018-10-29 18:30:44 -0500

    routed login,register

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'login',component:LoginComponent},
+{path:'register',component:RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingcomponents = [LoginComponent,RegisterComponent];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,18 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from  '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingcomponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    RegisterComponent,
-    LoginComponent
+    routingcomponents
   ],
   imports: [
     BrowserModule,
