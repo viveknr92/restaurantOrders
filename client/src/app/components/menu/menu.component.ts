fb1b34bc361968814b2d4af527823783fd8b9389
Patil <amp170830@utdallas.edu>
2018-11-09 14:46:39 -0600

    Cart html basic code

--- client/src/app/components/menu/menu.component.ts
+++ client/src/app/components/menu/menu.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FoodService } from 'src/app/services/food.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import {FlashMessagesService} from 'angular2-flash-messages/module';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,12 +11,51 @@ import { Router } from '@angular/router';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  q    :string;
+  type : string;
+  curPage : number;
+  pageLength :number;
+  uid : string;
+  imageToShow: any;
+  isImageLoading : boolean;
+  totalPages : number;
+  itemToBeDeleted : string;
+  todayDate: Date;
 
   public menu = [];
-  constructor(private _foodservice: FoodService, private _router: Router) { }
+  constructor(private _foodservice: FoodService,
+              private _router: Router,
+              private _authService: AuthService,
+              private _flashMessages : FlashMessagesService
+              ) { }
 
   ngOnInit() {
-    this._foodservice.getMenu()
+    this.type = null;
+		this.q = "";
+		this.pageLength =12;
+    this.fetchFoods();
+    // this._foodservice.getMenu()
+    // .subscribe(
+    //   data => this.menu = data,
+    //   err => {
+    //     if (err instanceof HttpErrorResponse){
+    //       if(err.status === 401){
+    //         this._router.navigate(['/login']);
+    //       }
+    //     }
+    //   });
+  }
+
+  fetchFoods(){
+		var q = this.q;
+		if(this.q == ""){
+			q = "all";
+		}
+    
+    var loopIdx = 0;
+		console.log("q "+ this.q + " type "+ this.type);
+
+		this._foodservice.getMenu()
     .subscribe(
       data => this.menu = data,
       err => {
@@ -24,6 +65,5 @@ export class MenuComponent implements OnInit {
           }
         }
       });
-  }
-
+	}
 }
