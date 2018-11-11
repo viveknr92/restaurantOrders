214e2716e96c164f90b32a0164e0041b51d3d68f
viveknr92 <32685466+viveknr92@users.noreply.github.com>
2018-11-11 13:18:16 -0600

    login failed - flash message

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,48 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from '../../services/auth.service';
+import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-import {FlashMessagesService} from 'angular2-flash-messages/module'
+import { FlashMessagesService } from 'angular2-flash-messages/module'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  providers:[AuthService]
+  providers: [AuthService]
 })
 export class LoginComponent implements OnInit {
-  user_name:string;
-  password:string;
+  user_name: string;
+  password: string;
 
   constructor(
-    private authservice:AuthService,
-    private route:Router,
-    private flashMessage : FlashMessagesService,
-    ) { }
+    private authservice: AuthService,
+    private route: Router,
+    private flashMessage: FlashMessagesService,
+  ) { }
 
   ngOnInit() {
   }
-  
-   Login(){
-    const loginInfo={
-      user_name:this.user_name,
-      password:this.password
-    }
-     console.log(loginInfo);
-     this.authservice.LoginUser(loginInfo)
-     .subscribe(res =>{
-       localStorage.setItem("token", res.token);
-       localStorage.setItem("user_id",res.user._id);
-       //console.log("HIII "+ res.user._id);
-       if(this.authservice.loggedIn()){
-        console.log("Login Success");
-        this.flashMessage.show("You are now logged in. Enjoy ordering", {cssClass : 'alert-success', timeout : 3000})
-        this.route.navigate(['menu']);
-       }else{
-         console.log(res);
-         console.log("Login Failed");
-         this.flashMessage.show("User not logged in",{cssClass : "alert-danger", timeout: 1000});
-       }}
-      )
 
-   }
+  Login() {
+    const loginInfo = {
+      user_name: this.user_name,
+      password: this.password
+    }
+    console.log(loginInfo);
+    this.authservice.LoginUser(loginInfo)
+
+      .subscribe(res => {
+        console.log("token");
+        localStorage.setItem("token", res.token);
+        localStorage.setItem("user_id", res.user._id);
+        console.log("Login Success");
+        this.flashMessage.show("You are now logged in. Enjoy ordering", { cssClass: 'alert-success', timeout: 3000 })
+        this.route.navigate(['menu']);
+      }, err => {
+        console.log("Login Failed");
+        console.log(err);
+        this.flashMessage.show("User not logged in", { cssClass: "alert-danger", timeout: 1000 });
+      })
+  }
 }
